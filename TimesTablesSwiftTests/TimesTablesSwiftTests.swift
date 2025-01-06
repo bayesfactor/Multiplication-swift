28176e79b7a125cea3af573acf2f07e554b15002
Tim <16023856+bayesfactor@users.noreply.github.com>
2025-01-05 20:09:18 -0800

    Create TimesTablesSwiftTests.swift

---
+++ TimesTablesSwiftTests/TimesTablesSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesSwiftTests.swift
+//  TimesTablesSwiftTests
+//
+//  Created by Tim Holme on 1/3/25.
+//
+
+import Testing
+@testable import TimesTablesSwift
+
+struct TimesTablesSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
