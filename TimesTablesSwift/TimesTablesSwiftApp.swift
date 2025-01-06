a728fd320a41b583905f39636f4732859b81eb8d
Tim <16023856+bayesfactor@users.noreply.github.com>
2025-01-05 20:08:02 -0800

    Create TimesTablesSwiftApp.swift

---
+++ TimesTablesSwift/TimesTablesSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesSwiftApp.swift
+//  TimesTablesSwift
+//
+//  Created by Tim Holme on 1/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
